745a714874d346836441d803568baa3cb4d4c0b8
Choi <tjddnr1104@gmail.com>
2024-02-14 09:32:34 +0900

    성욱 헤더, 메인, 페이지 연결

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import '../css/Main.css'
+import { Link } from 'react-router-dom'
 const Main = () => {
   return (
     <div id='mainContainer'>
       <div id='mainContent1'>
-        <h1>사진첩 정리 서비스 설명</h1>
-        <button>바로가기→</button>
-      </div>
-      <div id='mainContent2'>
-        <h1>SNS 서비스 설명!!!!</h1>
-        <button>바로가기→</button>
+        <div id='mainTextContainer'>
+          <strong id='mainText'>
+            PicStory와 함께
+            <br />
+            소중한 사진을 한 곳에서
+          </strong>
+          <p id='mainSubText'>
+            자동 태깅을 활용해 사진을 정리하세요.
+            <br />
+            태그기반 이미지 서치가 가능합니다.
+            <br />
+            태그를 기반으로 나만의 폴더를 만들 수 있습니다.
+          </p>
+          <Link to='/photoAlbum' id='goPhotoAlbumBtn'><span>PicStory▶</span></Link>
+        </div>
+        <div id='mainImgContainer'>
+          <img src="/images/mainimg.png" alt="홈메인이미지" id='mainImg'/>
+        </div>
       </div>
     </div>
   )
